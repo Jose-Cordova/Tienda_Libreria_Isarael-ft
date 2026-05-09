a5ad8a467bcf97fda3e27e76635f2251edea4617
José Córdova <josecordoba19@gmail.com>
2026-05-09 08:24:13 -0600

    Commit Inicial

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import Button from 'primevue/button'
+import Card from 'primevue/card'
+</script>
+
+<template>
+  <div class="min-h-screen bg-slate-900 flex items-center justify-center p-10">
+
+    <Card class="w-full max-w-md shadow-2xl">
+      <template #title>
+        <div class="text-center text-2xl font-bold">
+          🚀 TechStore UI Test
+        </div>
+      </template>
+
+      <template #content>
+        <p class="mb-6 text-center">
+          Si ves esto con estilo moderno,
+          PrimeVue y Tailwind están funcionando correctamente.
+        </p>
+
+        <div class="flex justify-center gap-4">
+          <Button label="Primario" icon="pi pi-check" />
+          <Button label="Secundario" severity="secondary" />
+        </div>
+      </template>
+    </Card>
+
+  </div>
+</template>
