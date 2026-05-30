f92aeacab43ec6d6a2d7be9cbf5793a1937d4ffd
AnthonyGuardado <mg25001@itcha.edu.sv>
2026-05-30 12:48:08 -0600

    Merge con RamaTony

--- src/views/venta/HistorialVentasView.vue
+++ src/views/venta/HistorialVentasView.vue
@@ -207,7 +207,7 @@ const ejecutarAnulacion = async () => {
 };
 
 // -----------------------------------------------
-// Carga inicial
+// Carga Inicial
 // -----------------------------------------------
 onMounted(() => {
   cargarMetodosPago();
