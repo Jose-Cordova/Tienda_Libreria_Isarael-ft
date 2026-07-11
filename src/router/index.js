5f96084c5b56cb068056d32cabfe48042f95ddde
AnthonyGuardado <mg25001@itcha.edu.sv>
2026-07-10 23:25:53 -0600

    Quitando ruta de unidad de medida

--- src/router/index.js
+++ src/router/index.js
@@ -59,11 +59,6 @@ const router = createRouter({
           component: () => import('../views/Marca.vue'),
         },
         {
-          path: 'unidades',
-          name: 'unidades',
-          component: () => import('../views/Unidad.vue'),
-        },
-        {
           path: 'proveedores',
           name: 'proveedores',
           component: () => import('../views/ProveedorView.vue')
